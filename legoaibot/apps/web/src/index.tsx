bfd74eb6f97acca7455d14bec94206d4a5a945ce
Daniel Fang <qkfang@hotmail.com>
2024-12-29 00:23:37 +1100

    font image

--- legoaibot/apps/web/src/index.tsx
+++ legoaibot/apps/web/src/index.tsx
@@ -1,43 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider, BrowserRouter as Router, Routes, Route  } from "react-router-dom";
 import { initializeIcons } from "@fluentui/react";
 import "./index.css";
 
 import Layout from "./pages/layout/Layout";
 import Chat from "./pages/chat/Chat";
+import Voice from "./pages/voice/Voice";
+import Brick from "./pages/brick/Brick";
+import Image from "./pages/image/Image";
+import About from "./pages/about/About";
+import Home from "./pages/home/Home";
 
-var layout;
-
-layout = <Layout />;
+var layout = <Layout />;
 
 initializeIcons();
-
-const router = createHashRouter([
-    {
-        path: "/",
-        element: layout,
-        children: [
-            {
-                index: true,
-                element: <Chat />
-            },
-            {
-                path: "*",
-                lazy: () => import("./pages/NoPage")
-            }
-        ]
-    }
-]);
 
 // ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 //         <RouterProvider router={router} />
 // );
 
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+            <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/brick" element={<Brick />} />
+          <Route path="/image" element={<Image />} />
+          <Route path="/voice" element={<Voice />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
+    </Router>
     </React.StrictMode>
 );
 
