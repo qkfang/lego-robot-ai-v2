7d70d604c8234d2de43bfbbc48120df4b4871398
Daniel Fang <qkfang@hotmail.com>
2025-01-12 09:43:55 +1100

    tide up chat folder

--- legoaibot/apps/chat/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "standalone",
-  experimental: {
-    serverComponentsExternalPackages: ["@azure/storage-blob"],
-  },
-};
-
-module.exports = nextConfig;
